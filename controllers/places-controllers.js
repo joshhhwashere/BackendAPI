8956b1adcf9a52f9d7de1d49c4d54de5db5649e4
joshwashere <joshua.gonzalez@tecnologicoliceocristiano.edu.ec>
2024-05-13 21:28:41 -0500

    add patch logic

---
+++ controllers/places-controllers.js
@@ -0,0 +1,78 @@
+
+const HttpError = require('../models/http-error');
+const uuid = require('uuid');
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Rotonda',
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Centro Cívico',
+        creator: 'u1'
+    }
+];
+
+const getAllPlaces = (req, res, next)=>{
+    res.json({places : DUMMY_PLACES});
+};
+
+const getPlacesById = (req, res, next) => {    
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === req.params.pid;
+    });
+    if (!place){        
+        const error = new Error('Lugar no existe para el id especificado');
+        error.code = 404;
+        next(error);
+    }
+    else{
+        res.json({place});
+    }    
+};
+
+const getPlacesByUsers = (req, res, next)=>{
+    const places = DUMMY_PLACES.find(p => {
+        return p.creator === req.params.uid
+    });    
+
+    if (!places){
+        const error = new HttpError('Lugar no existe para el id de usuario especificado', 404);
+        throw error;
+    }
+
+    res.json({places});
+};
+
+const postPlaces = (req, res, next)=>{
+    const {title, creator} = req.body;
+    const createdPlace = {
+        id: uuid.v4(),
+        title,
+        creator
+    };
+    DUMMY_PLACES.push(createdPlace);
+    res.status(201).json({place:createdPlace});
+};
+
+const updatePlaces = (req, res, next)=>{
+    const { title } = req.body;
+    const placesId = req.params.id;
+
+    const updatePlaces = {... DUMMY_PLACES.find(p => p.id === placesId)};
+    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+
+    updatedPlace.title = title;
+
+    DUMMY_PLACES[placeIndex] = updatePlace;
+
+    res.status(200).json({place: updatedPlace});
+};
+
+exports.getAllPlaces = getAllPlaces;
+exports.getPlacesById = getPlacesById;
+exports.getPlacesByUsers = getPlacesByUsers;
+exports.postPlaces = postPlaces;
+exports.updatedPlaces = updatePlaces;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,54 +1,17 @@
 const express = require('express');
 
+const placesControllers = require('../controllers/places-controllers');
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Rotonda',
-        creator: 'u1',
-    },
-    {
-        id: 'p2',
-        title: 'Centro Cívico',
+router.get('/', placesControllers.getAllPlaces);
 
-        creator: 'u1',
-    }
-];
+router.get('/:pid', placesControllers.getPlacesById);
 
-router.get('/', (req, res, next) => {
-    res.json({places : DUMMY_PLACES});
-});
+router.get('/users/:uid', placesControllers.getPlacesByUsers);
 
-router.get('/:pid', (req, res, next) => {
-        const places = DUMMY_PLACES.find(p => {
-            return p.id === req.params.pid;
-        });
-        res.json({places});
-    });
+router.post('/', placesControllers.postPlaces);
+
+router.patch('/pid',placesControllers.updatePlaces);
+
 module.exports = router;
-
-// app.get('/api/places', (req, res, next) => {
-//     const places = DUMMY_PLACES;
-//     res.send(places);
-//     next();
-// });
-
-// app.get('/api/places/:pid', (req, res, next) => {
-//     const places = DUMMY_PLACES.find(p => {
-//         return p.id === req.params.pid;
-//     });
-//     res.send(places);
-//     next();
-// });
-
-// app.get('/api/places/:ptitle', (req, res, next) => {
-    
-//     //console.log(req.params.title);
-
-//     const places = DUMMY_PLACES.find(p => {
-//         return p.title === req.params.ptitle;
-//     });
-//     res.send(places);
-//     next();
-// });
